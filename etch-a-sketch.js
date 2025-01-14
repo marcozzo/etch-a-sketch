7ad4f1aa2a1b4a34b3ad84b5d54b6450ed2470ff
marcozzodaro <130609450+marcozzo@users.noreply.github.com>
2025-01-14 20:51:18 -0300

    ADD LISTENER TO GRID SQUARES
    - on cursor hovering the color of the
    square will change.
    
    - the event handler executes a function
    which adds a class containing the new
    color styles.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,18 @@
 const gridContainer = document.querySelector('.flex-container');
 
+function changeColor(e){
+    let event = e.target;
+    let attribute = event.getAttribute('class');
+
+    if(  !(attribute.includes('color-changed') )  ){
+        attribute += ' color-changed';
+        event.setAttribute('class', attribute)
+    }
+}
+
 for (let i = 1; i <= 256; i++) {
     const gridSquare = document.createElement('div');
     gridContainer.appendChild(gridSquare)
     gridSquare.setAttribute('class', 'grid-item')
+    gridSquare.addEventListener('mouseover', changeColor)
 }
