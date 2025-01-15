79cf7edaeec857ec9f93cc6baca031ce92957853
marcozzodaro <130609450+marcozzo@users.noreply.github.com>
2025-01-15 14:11:55 -0300

    Add function for CHANGE SIZE button
    - add event handler to button and
    use changeSize as callback
    
    - changeSize function still not fully
    implemented

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,9 +10,26 @@ function changeColor(e){
     }
 }
 
-for (let i = 1; i <= 256; i++) {
-    const gridSquare = document.createElement('div');
-    gridContainer.appendChild(gridSquare)
-    gridSquare.setAttribute('class', 'grid-item')
-    gridSquare.addEventListener('mouseover', changeColor)
+
+
+    for (let i = 1; i <= 256; i++) {
+        const gridSquare = document.createElement('div');
+        gridContainer.appendChild(gridSquare)
+        gridSquare.setAttribute('class', 'grid-item')
+        gridSquare.addEventListener('mouseover', changeColor)
+        gridSquares = document.querySelectorAll('.grid-item')
+    }
+
+//TODAY'S TASK: ADD EVENT LISTENER TO BUTTON
+//(CHECK ETCH A SKETCH T.O.P PROJECT PAGE STEP 4)
+
+function changeSize(){
+    
+    let squareSize = prompt('Choose a size for your squares:\nMax 100');
+
+    gridContainer.textContent = "";
 }
+
+const gridButton = document.querySelector('.gridbutton');
+
+gridButton.addEventListener('click',changeSize)
